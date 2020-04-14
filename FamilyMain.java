d775fa65ad4bb0744caeea094315132d053df927
alarsen123 <63680257+alarsen123@users.noreply.github.com>
2020-04-14 15:27:00 -0700

    Add files via upload

---
+++ FamilyMain.java
@@ -0,0 +1,11 @@
+public class FamilyMain{
+
+   public static void main(String[] args){
+      Person HenryVIII = new Person();
+      
+      HenryVIII.getPaternal();
+      HenryVIII.getMaternal();
+      HenryVIII.getChildren();
+      
+   }
+}
--- Person.java
+++ Person.java
@@ -1,3 +1,34 @@
-public void Person{
+public class Person{
+
+   public Person() {
+   
+   }
+
+   private String paternal;
+   private String maternal;
+   private String child1;
+   private String child2;
+   private String child3;
+   
+   public void getPaternal() {
+      
+      paternal = "Henry VII";
+      System.out.println(paternal);
+      
+   }
+   
+   public void getMaternal() {
+   
+      maternal = "Elizabeth of York";
+   
+   }
+   
+   public void getChildren() {
+      
+      child1 = "Mary I";
+      child2 = "Elizabeth I";
+      child3 = "Edward VI";
+      
+   }
 
 }
